c85e7ea7c1d314421d388d9dcce8399c75ad088a
Calvin <calvinminhla@gmail.com>
2020-12-01 13:22:01 -0800

    Fixed warning regarding unique indexes for props object

--- app.js
+++ app.js
@@ -30,19 +30,18 @@
 
 /* Component Properties aka "props" */
 
-// const GroceryList = () => (
-//   <GroceryListItem groceryItems = {['Eggs', 'Bread']}/>
-// );
+const GroceryList = () => (
+  <GroceryListItem groceryItems = {['Eggs', 'Bread']}/>
+);
 
-// const GroceryListItem = (props) => (
-//   <ul>
-//     {/* Switch out the hardcode for something that can deconstruct the props */}
-//     <li>{props.groceryItems[0]}</li>
-//     <li>{props.groceryItems[1]}</li>
-//   </ul>
-// );
+const GroceryListItem = (props) => (
+  <ul>
+    <li>{props.groceryItems[0]}</li>
+    <li>{props.groceryItems[1]}</li>
+  </ul>
+);
 
-// ReactDOM.render(<GroceryList/>, document.getElementById('app'));
+ReactDOM.render(<GroceryList/>, document.getElementById('app'));
 
 /* Class components */
 
@@ -60,45 +59,45 @@
 
 // const GroceryList = (props) => (
 //   <ul>
-//     {props.groceryItems.map(item =>
-//       <GroceryListItem item={item} />
+//     {props.groceryItems.map((item, index) =>
+//       <GroceryListItem item={item} key={index}/>
 //     )}
 //   </ul>
 // );
-// // Fix warning in DevTools about having a unique key
+
 // ReactDOM.render(<GroceryList groceryItems={['Eggs', 'Bread']}/>, document.getElementById('app'));
 
 /* State */
 
-class GroceryListItem extends React.Component {
-  constructor(props) {
-    super(props);
+// class GroceryListItem extends React.Component {
+//   constructor(props) {
+//     super(props);
 
-    this.state = {
-      done: false
-    };
-  }
-  onListItemOver() {
-    this.setState({
-      done: !this.state.done
-    });
-  }
-  render() {
-    var style = {
-      fontWeight: this.state.done ? 'bold' : 'none'
-    };
-    return (
-      <li style={style} onMouseOver={this.onListItemOver.bind(this)}>{this.props.item}</li>
-    );
-  }
-}
+//     this.state = {
+//       done: false
+//     };
+//   }
+//   onListItemOver() {
+//     this.setState({
+//       done: !this.state.done
+//     });
+//   }
+//   render() {
+//     var style = {
+//       fontWeight: this.state.done ? 'bold' : 'none'
+//     };
+//     return (
+//       <li style={style} onMouseOver={this.onListItemOver.bind(this)}>{this.props.item}</li>
+//     );
+//   }
+// }
 
-const GroceryList = (props) => (
-  <ul>
-    {props.items.map(item =>
-      <GroceryListItem item={item} />
-    )}
-  </ul>
-);
-// Fix warning in DevTools about having a unique key
-ReactDOM.render(<GroceryList items={['Eggs', 'Bread']}/>, document.getElementById('app'));
+// const GroceryList = (props) => (
+//   <ul>
+//     {props.items.map((item, index) =>
+//       <GroceryListItem item={item} key={index}/>
+//     )}
+//   </ul>
+// );
+
+// ReactDOM.render(<GroceryList items={['Eggs', 'Bread']}/>, document.getElementById('app'));
